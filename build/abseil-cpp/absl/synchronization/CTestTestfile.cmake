b70cbef99a10729c2bd6181f22145d317b408cfa
peng <wangpeng187@126.com>
2026-04-20 06:44:48 +0000

    Stop tracking build directory

--- build/abseil-cpp/absl/synchronization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/gvins_ws/src/IC-GVINS/ic_gvins/thirdparty/abseil-cpp/absl/synchronization
-# Build directory: /root/gvins_ws/src/IC-GVINS/build/abseil-cpp/absl/synchronization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
